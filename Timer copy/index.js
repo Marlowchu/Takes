ef3df88d80efc97345e59e20630871cbd8c53696
Calvin Turner <turnerjrcalvin@gmail.com>
2022-02-16 21:47:48 -0500

    added modal

--- Timer copy/index.js
+++ Timer copy/index.js
@@ -1,22 +1,25 @@
-const express = require('express');
+
+// This app starts a server and listens on port 3000 for connections
 const app = express();
+// allows Node.js to transfer data over http
 const http = require('http');
+// creates HTTP server
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// passing it a string of HTML then sends a file to the client
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html'); 
 });
-
+// when the socket client is loaded on a page, it begins connecting a socket 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); 
+  console.log('a user is ready to make a Hot Take');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
-
-
+// app listening on port 3000
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
