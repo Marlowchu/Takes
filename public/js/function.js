6d05b0d740469da18856963292727ad7bc575761
Yasin Munye <cyasinsadiq@gmail.com>
2022-02-18 13:25:56 -0500

    adding a switch case and it pulls image for each case

--- public/js/function.js
+++ public/js/function.js
@@ -1,30 +1,60 @@
-const btn = document.querySelector('.addbutton');
-let count = 1; 
+const btn = document.querySelector(".addbutton");
+let count = 0;
 
-btn.addEventListener('click' ,() => {
-    count++;
-    console.log(count);
+btn.addEventListener("click", () => {
+  count++;
+  console.log(count);
+
+  const target = document.querySelector(".hierarchyPics");
+
   
-    switch (count) {
-      case 9:
-        console.log('9')
-        // target= InnerHtml="<img>Icon</img>"
-        break;
-      case '2':
-      case 'Papayas':
-        console.log('Mangoes and papayas are $2.79 a pound.');
-        // expected output: "Mangoes and papayas are $2.79 a pound."
-        break;
-      default:
-    //     console.log(`Sorry, we are out of ${expr}.`);
-    }
-  })
 
+  switch (count) {
+    case 1:
+    case 2:
+      console.log("Fan Ranking");
+      image= `<img src="./img/fan_image.png">`
+      
+      target.innerHTML = image;
+      // target= InnerHtml="<img>Icon</img>"
+      break;
+    case 3:
+    case 4:
+    // case 5:
+      console.log('Member Ranking');
+      
+      image= `<img src="./img/member_image.jpeg">`
+      target.innerHTML = image;
+      // expected output: "Mangoes and papayas are $2.79 a pound."
+      break;
+    case 5:
+    case 6:
+    // case 7:
+          console.log('Rising Star Ranking');
+          image= `<img src="./img/Rising_star.png">`
+      
+      target.innerHTML = image;
+      break;
+    case 7:
+    case 8:
+          console.log('Influencer Ranking')
+          image= `<img src="./img/influencer_image.png">`
+      
+      target.innerHTML = image;
+      break;
+    case 9:
+    case 10:
+          console.log('All Access Ranking')
+          image= `<img src="./img/all_access_image.png">`
+      
+      target.innerHTML = image;
+    default:
+      console.log(`All Access Ranking`);
+  }
+})
 
 // const number = `${count}`
 
-
-
 // const expr = 'Papayas';
 // switch (expr) {
 //   case 'Oranges':
@@ -39,7 +69,6 @@ btn.addEventListener('click' ,() => {
 //     console.log(`Sorry, we are out of ${expr}.`);
 // }
 
-
 // const expr = 'Papayas';
 // switch (expr) {
 //   case 'Oranges':
@@ -52,5 +81,3 @@ btn.addEventListener('click' ,() => {
 //     break;
 //   default:
 //     console.log(`Sorry, we are out of ${expr}.`);
-// }
-
